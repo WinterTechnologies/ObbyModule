617198abbf589ac9c2b09a259e85788f8971b7a7
flvffywvffy <flvffywvffy@gmail.com>
2023-02-08 19:32:03 +0000

    Added damagePlayer, icyPart, disapearingPart & fadePart functions.

--- src/ReplicatedStorage/Obby.lua
+++ src/ReplicatedStorage/Obby.lua
@@ -24,4 +24,42 @@ function Obby:damagePlayer(brick, amount)
 	end)
 end
 
+function Obby:icyPart(brick)
+    brick.CustomPhysicalProperties = PhysicalProperties.new(1, 0.01, 1, 100, 1)
+end
+
+function Obby:disapearingPart(brick, delay)
+    brick.Touched:Connect(function(h)
+        h = h.Parent:FindFirstChild("Humanoid")
+        if h ~= nil then
+            wait(delay)
+            brick.CanCollide = false
+            brick.Transparency = 1
+            wait(3)
+            brick.CanCollide = true
+            brick.Transparency = 0
+        end
+    end)
+end
+
+function Obby:fadePart(brick)
+    brick.Touched:Connect(function(h)
+        h = h.Parent:FindFirstChild("Humanoid")
+        if h ~= nil then
+            if not isTouched then
+                for count = 1, 10 do
+                    brick.Transparency = count / 10
+                    wait(0.1)
+                end
+
+                brick.CanCollide = false
+                wait(3)
+                brick.CanCollide = true
+                brick.Transparency = 0
+                isTouched = false
+            end
+        end
+    end)
+end
+
 return Obby
